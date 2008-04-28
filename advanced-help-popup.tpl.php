3a02ea45f8c629a49086e37fc3f6661ddbf34f16
merlinofchaos <merlinofchaos>
2008-04-28 18:48:07 +0000

    Allow line break filter; more complete help on .ini file.

--- advanced-help-popup.tpl.php
+++ advanced-help-popup.tpl.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
 
 <head>
-  <title><?php print $head_title; ?></title>
+  <title><?php print $title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
